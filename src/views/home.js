a11528e786b61a8d23d5669178d73a35a9a56eae
@ketan <35922826+Ketan987@users.noreply.github.com>
2023-06-06 18:26:21 +0000

    Update 5 files

--- src/views/home.js
+++ src/views/home.js
@@ -32,7 +32,7 @@ const Home = (props) => {
       <div className="home-hero">
         <div className="home-container01">
           <div className="home-card">
-            <h1 className="home-text HeadingOne">Are You Looking for Fund Manager?</h1>
+            <h1 className="home-text HeadingOne">Let AI be your fund Manager.</h1>
             <h1 className="home-text01 HeadingOne">Choose the best</h1>
             <span className="home-text02 Lead">
               {/* <span>
@@ -124,7 +124,7 @@ const Home = (props) => {
           <div className="home-container08">
             <img
               alt="image"
-              src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/laptop.jpg"
+              src="/playground_assets/upaiAsset01.jpeg"
               className="home-image05"
             />
             <span className="home-text09 Small">
@@ -137,7 +137,7 @@ const Home = (props) => {
             <div className="home-container09">
               <img
                 alt="image"
-                src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/coding.jpg"
+                src="/playground_assets/upaiAsset02.jpeg"
                 className="home-image06"
               />
             </div>
@@ -145,8 +145,9 @@ const Home = (props) => {
           <div className="home-container10">
             <img
               alt="image"
-              src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/soft-ui-design-system/assets/img/tasks.jpg"
+              src="/playground_assets/upaiAsset03.jpeg"
               className="home-image07"
+              height="600"
             />
             <div className="home-container11">
               <h3 className="HeadingTwo">
@@ -317,7 +318,7 @@ const Home = (props) => {
             </svg>
           </div>
           <h2 className="home-text30 HeadingTwo">
-            <span className="home-text31">Beware The Components</span>
+            <span className="home-text31">Get Familier With Your Components</span>
           </h2>
           <h3 className="home-text32 HeadingTwo">
             <span className="home-text33">How To Handle Them</span>
@@ -388,7 +389,7 @@ const Home = (props) => {
               <svg viewBox="0 0 1152 1024" className="home-icon4">
                 <path d="M1088 901.166c0 45.5 26.028 84.908 64 104.184v15.938c-10.626 1.454-21.472 2.224-32.5 2.224-68.008 0-129.348-28.528-172.722-74.264-26.222 6.982-54.002 10.752-82.778 10.752-159.058 0-288-114.616-288-256s128.942-256 288-256c159.058 0 288 114.616 288 256 0 55.348-19.764 106.592-53.356 148.466-6.824 14.824-10.644 31.312-10.644 48.7zM512 0c278.458 0 504.992 180.614 511.836 405.52-49.182-21.92-103.586-33.52-159.836-33.52-95.56 0-185.816 33.446-254.138 94.178-70.846 62.972-109.862 147.434-109.862 237.822 0 44.672 9.544 87.888 27.736 127.788-5.228 0.126-10.468 0.212-15.736 0.212-27.156 0-53.81-1.734-79.824-5.044-109.978 109.978-241.25 129.7-368.176 132.596v-26.916c68.536-33.578 128-94.74 128-164.636 0-9.754-0.758-19.33-2.164-28.696-115.796-76.264-189.836-192.754-189.836-323.304 0-229.75 229.23-416 512-416z"></path>
               </svg>
-              <h1 className="home-text40 HeadingOne">Talk and Meet!</h1>
+              <h1 className="home-text40 HeadingOne">Let's Talk!</h1>
               <span className="home-text41">
                 <span>Website visitors</span>
                 <span></span>
